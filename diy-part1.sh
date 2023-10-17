4678c74b14f2b28c2f13d53e3ba86e749aea9d9d
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2023-10-17 09:34:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,34 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# add helloworld
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+
+# theme argon
+ rm -rf  package/lean/luci-theme-argon 
+ git clone  https://github.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
+
+
+# aoo filter
+# echo "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
+
+# Add luci-app-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
+# add OpenClash
+# git clone https://github.com/vernesong/OpenClash package/OpenClash
+# add smartdns
+# svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns   package/luci-app-smartdns
+
+# Kernel版本控制
+# sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
+
+
+
+
+
+
+
+
