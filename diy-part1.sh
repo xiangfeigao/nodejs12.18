1833f00421309566429f1ec7cf15527448175a33
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2023-10-19 17:19:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
+# https://githubfast.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
@@ -14,29 +14,29 @@
 
 # add helloworld
  sed -i "/helloworld/d" "feeds.conf.default"
- echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+ echo "src-git helloworld https://githubfast.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 # theme argon
 # rm -rf  package/lean/luci-theme-argon 
-# git clone  https://github.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
+# git clone  https://githubfast.com/jerrykuku/luci-theme-argon  package/lean/luci-theme-argon
 
 
 # aoo filter
-# echo "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git" >> feeds.conf.default
+# echo "src-git OpenAppFilter https://githubfast.com/destan19/OpenAppFilter.git" >> feeds.conf.default
 
 # Add luci-app-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-# git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
+# git clone https://githubfast.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# git clone https://githubfast.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 # add OpenClash
-  git clone https://github.com/vernesong/OpenClash package/OpenClash
+  git clone https://githubfast.com/vernesong/OpenClash package/OpenClash
 # add smartdns
-# svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns   package/luci-app-smartdns
+# svn co https://githubfast.com/kenzok8/small-package/trunk/luci-app-smartdns   package/luci-app-smartdns
 
 # Kernel版本控制
- sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.15/g" target/linux/ramips/Makefile
+ sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
 # add frp
 # rm -rf  package/lean/frp
- # git clone https://github.com/fatedier/frp package/lean/frp
+ # git clone https://githubfast.com/fatedier/frp package/lean/frp
 
 
 
