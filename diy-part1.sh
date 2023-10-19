32795c53be8eed15d2029b56a67e0b7dd9c3909e
xiangfeigao <32321141+xiangfeigao@users.noreply.github.com>
2023-10-19 11:19:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -36,7 +36,7 @@
 # sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
 # add frp
 # rm -rf  package/lean/frp
- git clone https://github.com/fatedier/frp package/lean/frp
+ # git clone https://github.com/fatedier/frp package/lean/frp
 
 
 
